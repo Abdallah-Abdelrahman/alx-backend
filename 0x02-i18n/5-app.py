64300b9011fe901a3d97cd27b3cf2e82b3ae16f9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-03 20:35:41 +0800

    Debug 0x08-5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -28,9 +28,9 @@ babel = Babel(app)
 def get_locale():
     '''determine the best match with our supported languages'''
     lang = request.args.get('locale')
-    if lang in app.config['LANGUAGES']:
+    if lang in app.config.get('LANGUAGES'):
         return lang
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config.get('LANGUAGES'))
 
 
 @app.route('/')
@@ -52,7 +52,7 @@ def before_request() -> None:
     if id_ and id_.isnumeric():
         id_ = int(id_)
     # set user on flask global
-    g.user = get_user(id_)
+    setattr(g, 'user', get_user(id_))
 
 
 if __name__ == '__main__':
