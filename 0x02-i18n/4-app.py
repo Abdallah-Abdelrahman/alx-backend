22cc98db9fbaacf985bf03f4d6530b33ec786b71
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-03 17:19:29 +0800

    Check if request arguments has the requested lcoale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,10 +19,13 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     '''determine the best match with our supported languages'''
+    lang = request.args.get('locale')
+    if lang in app.config.get('LANGUAGES'):
+        return lang
     return request.accept_languages.best_match(app.config.get('LANGUAGES'))
 
 
 @app.route('/')
 def index():
     '''render index template'''
-    return render_template('3-index.html')
+    return render_template('4-index.html')
