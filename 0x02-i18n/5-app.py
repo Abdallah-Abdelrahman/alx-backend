de554db476114f0afcbbb213b2c9afd7a7395fb9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-03 20:31:36 +0800

    Debug 0x07-5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -28,9 +28,9 @@ babel = Babel(app)
 def get_locale():
     '''determine the best match with our supported languages'''
     lang = request.args.get('locale')
-    if lang in app.config.get('LANGUAGES'):
+    if lang in app.config['LANGUAGES']:
         return lang
-    return request.accept_languages.best_match(app.config.get('LANGUAGES'))
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
